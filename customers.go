5090a1352ac12158cf22e27da9bf32645f58a4aa
Karuppiah Natarajan <karuppiah7890@gmail.com>
2023-03-28 22:42:44 +0530

    return created customer as part of create customer api
    * create a type for namespace
    * add settings fields in create customer test
    * define different data types in settings as constants

--- customers.go
+++ customers.go
@@ -42,14 +42,21 @@ type Customer struct {
 type Setting struct {
 	Id        string          `json:"id"`
 	Value     string          `json:"value"`
-	Namespace string          `json:"namespace"`
+	Namespace Namespace       `json:"namespace"`
 	Name      string          `json:"name"`
 	DataType  SettingDataType `json:"dataType"`
 }
 
+type Namespace string
+
+const (
+	UserNamespace  Namespace = "USER"
+	TogaiNamespace Namespace = "TOGAI"
+)
+
 type SettingDataType string
 
-var (
+const (
 	StringSettingDataType    SettingDataType = "STRING"
 	NumericSettingDataType   SettingDataType = "NUMERIC"
 	JsonSettingDataType      SettingDataType = "JSON"
@@ -72,23 +79,61 @@ type Account struct {
 	Settings []Setting `json:"settings"`
 }
 
+type CreateCustomerOutput struct {
+	// Customer identifier
+	Id string `json:"id"`
+	// Name of the Customer
+	Name string `json:"name"`
+	// Primary email of the customer
+	PrimaryEmail string `json:"primaryEmail"`
+	// Billing address of the customer
+	BillingAddress string `json:"billingAddress"`
+	// Customer Settings
+	Settings []Setting `json:"settings"`
+	// Payload to create [Account]
+	Account CreateAccountOutput `json:"account"`
+}
+
+type CreateAccountOutput struct {
+	// Identifier of the account
+	Id string `json:"id"`
+	// Name of the Account
+	Name string `json:"name"`
+	// Status of the account
+	Status string `json:"status"`
+	// Use [ISO 4217] currency code in which the account must be invoiced.
+	// For example: AED is the currency code for United Arab Emirates dirham.
+	//
+	// [ISO 4217]: https://en.wikipedia.org/wiki/ISO_4217
+	InvoiceCurrency string `json:"invoiceCurrency"`
+	// Aliases are tags that are associated with an account. Multiple aliases are allowed for a single account.
+	Aliases []AccountAlias `json:"aliases"`
+	// Account Settings
+	Settings []Setting `json:"settings"`
+}
+
+type AccountAlias struct {
+	Alias  string `json:"alias"`
+	Status string `json:"status"`
+}
+
 // TODO: Move URL and Token to togai client struct.
 // Convert this function into a method on togai client struct and use url and token from there.
 // Use http client from the togai client struct
-func (c *TogaiClient) CreateCustomer(customer Customer) error {
+func (c *TogaiClient) CreateCustomer(customer Customer) (*CreateCustomerOutput, error) {
 	customersEndpoint, err := url.JoinPath(c.ApiBaseUrl, "customers")
 	if err != nil {
-		return fmt.Errorf("error forming customers API endpoint: %v", err)
+		return nil, fmt.Errorf("error forming customers API endpoint: %v", err)
 	}
 
 	createCustomerJsonPayload, err := json.Marshal(customer)
 	if err != nil {
-		return fmt.Errorf("error serializing customer object to JSON string: %v", err)
+		return nil, fmt.Errorf("error serializing customer object to JSON string: %v", err)
 	}
 
 	req, err := http.NewRequest(http.MethodPost, customersEndpoint, bytes.NewReader(createCustomerJsonPayload))
 	if err != nil {
-		return fmt.Errorf("error occurred while forming request: %v", err)
+		return nil, fmt.Errorf("error occurred while forming request: %v", err)
 	}
 
 	req.Header.Add(ACCEPT_HTTP_HEADER, JSON_TYPE)
@@ -97,34 +142,27 @@ func (c *TogaiClient) CreateCustomer(customer Customer) error {
 
 	res, err := c.httpClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("error occurred while sending request: %v", err)
-	}
-
-	if res.StatusCode != 201 {
-		defer res.Body.Close()
-		body, _ := io.ReadAll(res.Body)
-		return fmt.Errorf("expected 201 created response but got: \nstatus code: %v, status: %v, body: %v", res.StatusCode, res.Status, string(body))
+		return nil, fmt.Errorf("error occurred while sending request: %v", err)
 	}
 
 	defer res.Body.Close()
 
-	// TODO: Should we return the created customer? Hmm
+	if res.StatusCode != 201 {
+		body, _ := io.ReadAll(res.Body)
+		return nil, fmt.Errorf("expected 201 created response but got: \nstatus code: %v, status: %v, body: %v", res.StatusCode, res.Status, string(body))
+	}
 
-	// TODO: Created Customer Response body is different from Create Customer Request body.
-	// So, we need to use different types here, for request and for response. In response, the
-	// aliases is an array of objects and not an array of strings like in request body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while reading response body: %v", err)
+	}
 
-	// body, err := io.ReadAll(res.Body)
-	// if err != nil {
-	// 	return fmt.Errorf("error occurred while reading response body: %v", err)
-	// }
+	var createdCustomer CreateCustomerOutput
 
-	// var createdCustomer Customer
+	err = json.Unmarshal(body, &createdCustomer)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while parsing json response body: %v\n\njson body: %v", err, string(body))
+	}
 
-	// err = json.Unmarshal(body, &createdCustomer)
-	// if err != nil {
-	// 	return fmt.Errorf("error occurred while parsing json response body: %v\n\njson body: %v", err, string(body))
-	// }
-
-	return nil
+	return &createdCustomer, nil
 }
--- customers_test.go
+++ customers_test.go
@@ -17,18 +17,38 @@ func TestCustomers(t *testing.T) {
 	t.Run("create customer", func(t *testing.T) {
 		randomNumber := rand.Int()
 		customer := togai.Customer{
-			Id:             fmt.Sprintf("test-customer-%d", randomNumber),
-			Name:           fmt.Sprintf("Test Customer %d", randomNumber),
-			PrimaryEmail:   "karuppiah+test-customer@togai.com",
+			Id:           fmt.Sprintf("test-customer-%d", randomNumber),
+			Name:         fmt.Sprintf("Test Customer %d", randomNumber),
+			PrimaryEmail: "karuppiah+test-customer@togai.com",
+			Settings: []togai.Setting{
+				{
+					Id:        fmt.Sprintf("dummy-customer-setting-%d", randomNumber),
+					Name:      fmt.Sprintf("Dummy Customer Setting %d", randomNumber),
+					Namespace: togai.UserNamespace,
+					Value:     "10",
+					DataType:  togai.NumericSettingDataType,
+				},
+			},
 			BillingAddress: "Test Billing Address",
 			Account: togai.Account{
 				Id:              fmt.Sprintf("test-account-%d", randomNumber),
 				Name:            fmt.Sprintf("Test Account %d", randomNumber),
 				InvoiceCurrency: "USD",
 				Aliases:         []string{fmt.Sprintf("test-account-%d", randomNumber)},
+				Settings: []togai.Setting{
+					{
+						Id:        fmt.Sprintf("dummy-account-setting-%d", randomNumber),
+						Name:      fmt.Sprintf("Dummy Account Setting %d", randomNumber),
+						Namespace: togai.UserNamespace,
+						Value:     fmt.Sprintf("something %d", randomNumber),
+						DataType:  togai.StringSettingDataType,
+					},
+				},
 			},
 		}
-		err := c.CreateCustomer(customer)
+		// TODO: Check if the output response fields and input request fields match, and that response also has
+		// some expected values like status as ACTIVE for account and account alias
+		_, err := c.CreateCustomer(customer)
 		if err != nil {
 			t.Fatalf("expected no error but an error occurred: %v", err)
 		}
